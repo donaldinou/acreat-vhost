c61b319526bcdf8b870e7b7c8bd4bf620436db22
Adrien Loyant <loyant@acreat.com>
2017-02-15 18:03:13 +0100

    add symlinks

--- bin/a2enall.sh
+++ bin/a2enall.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-cd "../sites-enabled"
-sites="../sites-available"
-for file in $sites* ; do
-    if [ -h $file ]
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+sites=${DIR}"/../sites-available"
+
+cd ${DIR}"/../sites-enabled"
+for file in ${sites}/* ; do
+    filename=$(basename $file)
+    if [ -h $filename ]
     then
-        rm -f $file
+        rm -f ${filename}
     fi
-    ln -s ../sites-available/${file} ${file}
+    ln -s ../sites-available/${filename} ${filename}
 done
