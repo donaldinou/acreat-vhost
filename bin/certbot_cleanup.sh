5b50a10930a0e111a586b1c03d31d7a3b3fa848a
Adrien Loyant <loyant@acreat.com>
2019-08-27 07:12:39 +0000

    norefs : *moins*

--- bin/certbot_cleanup.sh
+++ bin/certbot_cleanup.sh
@@ -3,4 +3,6 @@ CERTBOT_PATH=/var/share/drupal/.well-known/acme-challenge
 rm -f $CERTBOT_PATH/$CERTBOT_TOKEN
 
 # push certificates to amazon iam
+import-certificates-to-aws upload --domain=$CERTBOT_DOMAIN
+import-certificates-to-aws attach --domain=$CERTBOT_DOMAIN
 #aws iam upload-server-certificate --server-certificate-name $CERTBOT_DOMAIN --private-key file:///var/share/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem --certificate-body file:///var/share/letsencrypt/live/$CERTBOT_DOMAIN/cert.pem --certificate-chain file:///var/share/letsencrypt/live/$CERTBOT_DOMAIN/chain.pem
